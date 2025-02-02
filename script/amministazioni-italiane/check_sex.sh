ce26db24ba64147be957d5d2906b2be3ee8c0f48
aborruso <aborruso@gmail.com>
2025-02-02 12:02:36 +0100

    update

---
+++ script/amministazioni-italiane/check_sex.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -x
+#set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "${folder}"/tmp
+
+# svuota la cartella senza eliminarla
+find "${folder}"/tmp -mindepth 1 -delete
+
+# trova tutti i file CSV che non sono valorizzati con sesso
+grep -L 'sesso' "${folder}"/../../dati/amministazioni-italiane/processing/*.csv >"${folder}"/tmp/dacontrollare.txt
+
+while read -r line; do
+  nome=$(basename "${line}" .csv)
+  mlr --c2n cut -f nome then uniq -a "${line}" >"${folder}"/tmp/nomi_"${nome}".txt
+  sesso.sh -f "${folder}"/tmp/nomi_"${nome}".txt >"${folder}"/tmp/sesso_"${nome}".jsonl
+  mlr --ijsonl --ocsv cat "${folder}"/tmp/sesso_"${nome}".jsonl >"${folder}"/tmp/sesso_"${nome}".csv
+  mlr --csv join -j nome -f "${line}" then unsparsify "${folder}"/tmp/sesso_"${nome}".csv >"${folder}"/tmp/"${nome}"_sesso.csv
+
+  cp "${folder}"/tmp/"${nome}"_sesso.csv "${line}"
+done <"${folder}"/tmp/dacontrollare.txt
+
+# svuota la cartella senza eliminarla
+find "${folder}"/tmp -mindepth 1 -delete
+
+# per ogni file in "${folder}"/../../dati/amministazioni-italiane/processing/*.csv conta quante righe
+for file in "${folder}"/../../dati/amministazioni-italiane/processing/*.csv; do
+  nome=$(basename "${file}" .csv)
+  conteggio=$(mlr --c2n cut -f sesso then filter 'is_null($sesso)' "${file}" | wc -l)
+  if [ "${conteggio}" -gt 0 ]; then
+    echo "${file}" >>"${folder}"/tmp/dacontrollare.txt
+  fi
+done
+
+while read -r line; do
+  nome=$(basename "${line}" .csv)
+  mlr --c2n cut -f nome,sesso then filter 'is_null($sesso)' then uniq -a then cut -f nome "${line}" >"${folder}"/tmp/nomi_"${nome}".txt
+  sesso.sh -f "${folder}"/tmp/nomi_"${nome}".txt >"${folder}"/tmp/sesso_"${nome}".jsonl
+  sleep 5
+done <"${folder}"/tmp/dacontrollare.txt
